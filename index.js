d69ebbcbf12f39dcc5902ff28e3849d884d1d65a
Tyan89@list.ru <Tyan89@list.ru>
2021-02-20 15:07:37 +0300

    2

--- index.js
+++ index.js
@@ -7,24 +7,63 @@ window.onload = function () {
   })
 
   // Add animation bindings to elements
-  lax.addElements('.title', {
-      scrollY: {                // Driver name
-        translateX: [           // CSS property
-          [0, 150],  // Driver value map
-          ['screenWidth', 0],   // Animation value map
-          {
-            inertia: 10        // Options
-          }
+  lax.addElements('.title1', {
+    scrollY: {
+        translateX: [
+            ["elOutY", "elCenterY", "elInY"],
+            [200, 'screenWidth/2', 'screenWidth'],
+        ],
+        blur: [
+            [0, 100],
+            [30, 0]
         ],
 
-      }
-  })
-  lax.addElements('.title-2', {
-    scrollY: {
-      translateX: [
-        [0, 150],  // Driver value map
-        [0, 'elCenterY'],   // Animation value map
-      ]
     }
-  })
+})
+
+lax.addElements('.title2', {
+    scrollY: {
+        translateX: [
+            ["elOutY", "elCenterY", "elInY"],
+            ['screenWidth', 'screenWidth/4', 'screenWidth/4'],
+        ],
+        scaleX: [
+            [0, 100, 400],
+            [15, 15, 1],
+        ],
+        skewY: [
+            ["elOutY", "elCenterY", "elInY"],
+            [10, 1, 1],
+        ]
+
+    }
+})
+
+lax.addElements('.title3', {
+    scrollY: {
+        scale: [
+            [0, 1200],
+            [2, 1],
+        ],
+        translateX: [
+            ["elOutY", "elCenterY", "elInY"],
+            [10, 80, 300],
+        ],
+
+    }
+})
+
+lax.addElements('.text', {
+    scrollY: {
+        rotate: [
+            ["elOutY", "elCenterY", "elInY"],
+            [120, 0, 0],
+        ],
+        blur: [
+            ["elOutY", "elCenterY", "elInY"],
+            [10, 0, 30],
+        ],
+
+    }
+})
 }
